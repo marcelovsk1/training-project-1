03b4ddd599bdee462375c517f90a7e797b5716fc
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-16 15:54:03 -0300

    Initial Commit

---
+++ WeatherAPP2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherAPP2
+//
+//  Created by Marcelo Amaral Alves on 2023-11-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemIndigo)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherAPP2/WeatherAPP2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPP2App.swift
+//  WeatherAPP2
+//
+//  Created by Marcelo Amaral Alves on 2023-11-16.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPP2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
