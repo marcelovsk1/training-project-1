2cc1139d6289371bb1020aeda678365a40860a17
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-16 16:26:26 -0300

    creating the WeatherDayView

---
+++ WeatherAPP2/WeatherDayView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherDayView.swift
+//  WeatherAPP2
+//
+//  Created by Marcelo Amaral Alves on 2023-11-16.
+//
+
+import SwiftUI
+
+struct WeatherDayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherDayView()
+}
